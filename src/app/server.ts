77113be04eccea0f2dfcfc96bd752ef7cc333291
Martian Jeirgif <mgj.exe@gmail.com>
2025-03-18 20:02:39 +0530

    cors fix

--- src/app/server.ts
+++ src/app/server.ts
@@ -8,8 +8,28 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+
+// List of allowed origins
+const allowedOrigins = ["http://localhost:4200", "https://m9j.github.io"];
+
+// Dynamically set the origin
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps or CURL requests)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
+    // credentials: true, // If you need cookies or authentication tokens
+  })
+);
+
 app.use("/", router);
-app.use(cors());
 
 logger("Server starting...");
 logger("AI Services initializing...");
