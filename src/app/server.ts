9a551a0cc42990e8b22b147076faed3eb57ec1b5
Martian Jeirgif <mgj.exe@gmail.com>
2025-03-18 20:31:32 +0530

    https enablement

--- src/app/server.ts
+++ src/app/server.ts
@@ -3,9 +3,13 @@ import express from "express";
 import { initializeAIServices } from "./ai/initializer.ts";
 import router from "./routes.ts";
 import { logger } from "./utils.ts";
+import fs from "fs";
+import https from "https";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const rootPath = process.cwd();
 
 app.use(express.json());
 
@@ -36,7 +40,16 @@ logger("AI Services initializing...");
 await initializeAIServices();
 logger("AI Services initialized.");
 
+const httpsOptions = {
+  key: fs.readFileSync(path.join(rootPath, `/certs/server.key`)), // Path to your private key
+  cert: fs.readFileSync(path.join(rootPath, `/certs/server.cert`)), // Path to your certificate
+};
+
 app.listen(PORT, () => {
   logger("Server started...");
   logger(`Server running on http://<IP_ADDRESS>:${PORT}`);
+});
+
+https.createServer(httpsOptions, app).listen(443, () => {
+  console.log("HTTPS Server running on port 443");
 });
